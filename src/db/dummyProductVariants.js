36624373891deb0d76b5e9ffe140c994a7c83ada
Galang Yoga <galangkgz@gmail.com>
2026-06-10 14:22:37 +0700

    Add dummy product variants

---
+++ src/db/dummyProductVariants.js
@@ -0,0 +1,34 @@
+export const dummyProductVariants = [
+  {
+    id: 1,
+    productId: 6,
+    name: "1 pcs",
+    qtyMultiplier: 1,
+    price: 3000,
+    active: true,
+  },
+  {
+    id: 2,
+    productId: 7,
+    name: "1 ball",
+    qtyMultiplier: 20,
+    price: 55000,
+    active: true,
+  },
+  {
+    id: 3,
+    productId: 9,
+    name: "1 pcs",
+    qtyMultiplier: 1,
+    price: 1000,
+    active: true,
+  },
+  {
+    id: 4,
+    productId: 9,
+    name: "Paket 10 pcs",
+    qtyMultiplier: 10,
+    price: 9000,
+    active: true,
+  },
+];
